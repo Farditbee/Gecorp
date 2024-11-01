455cdae99ad11a121063f041c7a9129e2bfcac24
Farditt <farhandito15@gmail.com>
2024-11-01 10:28:28 +0700

    added : DetailKasir Model

---
+++ app/Models/DetailKasir.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DetailKasir extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'detail_kasir';
+
+    protected $guarded = [''];
+
+    public $incrementing = false;
+
+    public $timestamps = false;
+
+    protected $keyType = 'string';
+
+    public $primaryKey = 'id';
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
