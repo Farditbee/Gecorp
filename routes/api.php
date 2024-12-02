6b444c10729dfe42eb860324954dca4b1ffd3b2d
Bintang0501 <gilangsuryawicaksono@gmail.com>
2024-12-02 11:00:33 +0700

    api

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\KasirController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/print/{id}', [KasirController::class, 'cetakEppos']);
