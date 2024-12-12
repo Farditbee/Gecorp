f1887b4c38206158b7865401c65f73daff243234
Farditbee <farhandito15@gmail.com>
2024-12-12 15:18:39 +0700

    add api usercontroller

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KasirController;
 use App\Http\Controllers\PembelianBarangController;
 use App\Http\Controllers\RatingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +13,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/print/{id}', [KasirController::class, 'cetakEppos']);
+
 Route::get('/dashboard', [DashboardController::class, 'laporan_kasir'])->name('master.index.kasir');
 Route::get('/get-barang-jual', [DashboardController::class, 'getBarangJual'])->name('dashboard.rating');
+
 Route::get('/getpembelianbarang', [PembelianBarangController::class, 'getpembelianbarang'])->name('master.pembelian.get');
+Route::get('/getdatauser', [UserController::class, 'getdatauser'])->name('master.getdatauser');
