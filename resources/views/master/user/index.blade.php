96b9cda03b49cc103077718a8079431200150aab
Eko748 <permanaeko74@gmail.com>
2024-12-13 13:04:56 +0700

    fix(master toko)

--- resources/views/master/user/index.blade.php
+++ resources/views/master/user/index.blade.php
@@ -32,11 +32,11 @@
                             <div class="d-flex mb-2 mb-lg-0">
                                 @if (Auth::user()->id_level == 1)
                                     <a href="{{ route('master.user.create') }}" class="mr-2 btn btn-primary">
-                                        <i class="ti-plus menu-icon"></i> Tambah
+                                        <i class="fa fa-circle-plus"></i> Tambah
                                     </a>
                                 @else
                                     <a href="{{ route('master.user.create') }}" class="mr-2 btn btn-secondary disabled">
-                                        <i class="ti-plus menu-icon"></i> Tambah
+                                        <i class="fa fa-circle-plus"></i> Tambah
                                     </a>
                                 @endif
                             </div>
@@ -94,6 +94,10 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('asset_js')
+    <script src="{{ asset('js/pagination.js') }}"></script>
 @endsection
 
 @section('js')
@@ -229,56 +233,6 @@
             renderPagination();
         }
 
-        function renderPagination() {
-            let paginationHtml = '';
-
-            if (currentPage > 1) {
-                paginationHtml +=
-                    `<button class="paginate-btn prev-btn btn btn-sm btn-outline-primary mx-1" data-page="${currentPage - 1}"><i class="fa fa-circle-chevron-left"></i></button>`;
-            }
-
-            let startPage = Math.max(1, currentPage - 2);
-            let endPage = Math.min(totalPage, currentPage + 2);
-
-            if (startPage > 1) {
-                paginationHtml += `<button class="paginate-btn page-btn btn btn-sm btn-primary" data-page="1">1</button>`;
-                if (startPage > 2) {
-                    paginationHtml +=
-                        `<button class="btn btn-sm btn-primary" style="pointer-events: none;"><i class="fa fa-ellipsis"></i></button>`;
-                }
-            }
-
-            for (let i = startPage; i <= endPage; i++) {
-                paginationHtml +=
-                    `<button class="paginate-btn page-btn btn btn-sm btn-primary ${i === currentPage ? 'active' : ''}" data-page="${i}">${i}</button>`;
-            }
-
-            if (endPage < totalPage) {
-                if (endPage < totalPage - 1) {
-                    paginationHtml +=
-                        `<button class="btn btn-sm btn-primary" style="pointer-events: none;"><i class="fa fa-ellipsis"></i></button>`;
-                }
-                paginationHtml +=
-                    `<button class="paginate-btn page-btn btn btn-sm btn-primary" data-page="${totalPage}">${totalPage}</button>`;
-            }
-
-            if (currentPage < totalPage) {
-                paginationHtml +=
-                    `<button class="paginate-btn next-btn btn btn-sm btn-outline-primary mx-1" data-page="${currentPage + 1}"><i class="fa fa-circle-chevron-right"></i></button>`;
-            }
-
-            $('#pagination-js').html(paginationHtml);
-
-            $('#pagination-js').off('click', '.paginate-btn').on('click', '.paginate-btn', async function(e) {
-                const newPage = parseInt($(this).data('page'));
-                if (!isNaN(newPage)) {
-                    currentPage = newPage;
-                    await getListData(defaultLimitPage, currentPage, defaultAscending, defaultSearch,
-                        customFilter);
-                }
-            });
-        }
-
         async function deleteData() {
             $(document).on("click", ".hapus-data", async function() {
                 isActionForm = "destroy";
@@ -322,29 +276,8 @@
 
         async function initPageLoad() {
             await getListData(defaultLimitPage, currentPage, defaultAscending, defaultSearch, customFilter);
-            $('#limitPage').on('change', async function() {
-                defaultLimitPage = parseInt($(this).val());
-                currentPage = 1;
-                await getListData(defaultLimitPage, currentPage, defaultAscending, defaultSearch,
-                    customFilter);
-            });
-
-            $('.tb-search').on('input', debounce(async () => {
-                defaultSearch = $('.tb-search').val();
-                currentPage = 1;
-                console.log('di klik')
-                await getListData(defaultLimitPage, currentPage, defaultAscending, defaultSearch,
-                    customFilter);
-            }, 500));
+            await searchList();
             await deleteData();
-        }
-
-        function debounce(func, wait) {
-            let timeout;
-            return function(...args) {
-                clearTimeout(timeout);
-                timeout = setTimeout(() => func.apply(this, args), wait);
-            };
         }
     </script>
 @endsection
