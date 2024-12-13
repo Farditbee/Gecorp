6f3f926af8565f1122f56b7d9577ecfd27d36440
Farditbee <farhandito15@gmail.com>
2024-12-13 20:30:03 +0700

    update : supplier fix

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -9,6 +9,75 @@ use Illuminate\Support\Facades\DB;
 
 class SupplierController extends Controller
 {
+    public function getsupplier(Request $request)
+    {
+        $meta['orderBy'] = $request->ascending ? 'asc' : 'desc';
+        $meta['limit'] = $request->has('limit') && $request->limit <= 30 ? $request->limit : 30;
+
+        $query = Supplier::query();
+
+        $query->with([])->orderBy('id', $meta['orderBy']);
+
+        if (!empty($request['search'])) {
+            $searchTerm = trim(strtolower($request['search']));
+
+            $query->where(function ($query) use ($searchTerm) {
+                // Pencarian pada kolom langsung
+                $query->orWhereRaw("LOWER(nama_supplier) LIKE ?", ["%$searchTerm%"]);
+                $query->orWhereRaw("LOWER(contact) LIKE ?", ["%$searchTerm%"]);
+                $query->orWhereRaw("LOWER(alamat) LIKE ?", ["%$searchTerm%"]);
+                $query->orWhereRaw("LOWER(email) LIKE ?", ["%$searchTerm%"]);
+            });
+        }
+
+        if ($request->has('startDate') && $request->has('endDate')) {
+            $startDate = $request->input('startDate');
+            $endDate = $request->input('endDate');
+
+            // Lakukan filter berdasarkan tanggal
+            $query->whereBetween('created_at', [$startDate, $endDate]);
+        }
+
+        $data = $query->paginate($meta['limit']);
+
+        $paginationMeta = [
+            'total'        => $data->total(),
+            'per_page'     => $data->perPage(),
+            'current_page' => $data->currentPage(),
+            'total_pages'  => $data->lastPage()
+        ];
+
+        $data = [
+            'data' => $data->items(),
+            'meta' => $paginationMeta
+        ];
+
+        if (empty($data['data'])) {
+            return response()->json([
+                'status_code' => 400,
+                'errors' => true,
+                'message' => 'Tidak ada data'
+            ], 400);
+        }
+
+        $mappedData = collect($data['data'])->map(function ($item) {
+            return [
+                'id' => $item['id'],
+                'nama' => $item->nama_supplier,
+                'email' => $item->email,
+                'alamat' => $item->alamat,
+                'kontak' => $item->contact,
+            ];
+        });
+
+        return response()->json([
+            'data' => $mappedData,
+            'status_code' => 200,
+            'errors' => true,
+            'message' => 'Sukses',
+            'pagination' => $data['meta']
+        ], 200);
+    }
 
     public function index()
     {
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\KasirController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\PembelianBarangController;
 use App\Http\Controllers\RatingController;
+use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\TokoController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -24,3 +25,4 @@ Route::get('/getpembelianbarang', [PembelianBarangController::class, 'getpembeli
 Route::get('/getdatauser', [UserController::class, 'getdatauser'])->name('master.getdatauser');
 Route::get('/gettoko', [TokoController::class, 'gettoko'])->name('master.gettoko');
 Route::get('/getmember', [MemberController::class, 'getmember'])->name('master.getmember');
+Route::get('/getsupplier', [SupplierController::class, 'getsupplier'])->name('master.getsupplier');
