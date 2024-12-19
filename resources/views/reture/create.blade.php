456ff05092e8137b890df25a93d82241e7582a64
Eko748 <permanaeko74@gmail.com>
2024-12-19 16:47:47 +0700

    fix(input reture)

--- resources/views/reture/create.blade.php
+++ resources/views/reture/create.blade.php
@@ -23,7 +23,7 @@
                         <x-adminlte-alerts />
                         <div class="card-body table-border-style">
                             <div class="table-responsive">
-                                <form action="{{ route('reture.store') }}" method="post" class="">
+                                <form action="{{ route('reture.store') }}" method="post" class="" id="retureForm">
                                     @csrf
                                     <div class="row">
                                         <div class="col-12">
@@ -32,7 +32,8 @@
                                             <div class="input-group mb-6">
                                                 <input id="search-data" type="text" class="form-control"
                                                     placeholder="Masukkan / scan Qr Code Barang"
-                                                    aria-label="Recipient's username" aria-describedby="basic-addon2">
+                                                    aria-label="Recipient's username" aria-describedby="basic-addon2"
+                                                    required>
                                                 <div class="input-group-append">
                                                     <button id="btn-search-data" class="btn btn-primary" type="button"><i
                                                             class="fa fa-magnifying-glass mr-2"></i>Cari</button>
@@ -45,14 +46,14 @@
                                             <div class="form-group">
                                                 <label for="nama_toko" class="form-control-label">Nama Toko</label>
                                                 <input type="text" name="nama_toko" id="nama_toko" class="form-control"
-                                                    readonly>
+                                                    readonly required>
                                             </div>
                                         </div>
                                         <div class="col-3">
                                             <div class="form-group">
                                                 <label for="id_transaksi" class="form-control-label">ID Transaksi</label>
                                                 <input type="text" name="id_transaksi" id="id_transaksi"
-                                                    class="form-control" readonly>
+                                                    class="form-control" readonly required>
                                             </div>
                                         </div>
                                         <div class="col-3">
@@ -60,14 +61,14 @@
                                                 <label for="tipe_transaksi" class="form-control-label">Tipe
                                                     Transaksi</label>
                                                 <input type="text" name="tipe_transaksi" id="tipe_transaksi"
-                                                    class="form-control" readonly>
+                                                    class="form-control" readonly required>
                                             </div>
                                         </div>
                                         <div class="col-3">
                                             <div class="form-group">
                                                 <label for="nama_member" class="form-control-label">Nama Member</label>
                                                 <input type="text" name="nama_member" id="nama_member"
-                                                    class="form-control" readonly>
+                                                    class="form-control" readonly required>
                                             </div>
                                         </div>
                                     </div>
@@ -76,21 +77,21 @@
                                             <div class="form-group">
                                                 <label for="harga_jual" class="form-control-label">Harga Jual</label>
                                                 <input type="text" name="harga_jual" id="harga_jual" class="form-control"
-                                                    readonly>
+                                                    readonly required>
                                             </div>
                                         </div>
                                         <div class="col-4">
                                             <div class="form-group">
                                                 <label for="nama_barang" class="form-control-label">Nama Barang</label>
                                                 <input type="text" name="nama_barang" id="nama_barang"
-                                                    class="form-control" readonly>
+                                                    class="form-control" readonly required>
                                             </div>
                                         </div>
                                         <div class="col-4">
                                             <div class="form-group">
                                                 <label for="qty" class="form-control-label">Qty</label>
                                                 <input type="text" name="qty" id="qty" class="form-control"
-                                                    readonly>
+                                                    required>
                                             </div>
                                         </div>
                                     </div>
@@ -134,13 +135,23 @@
                 return resp;
             });
 
-            if (getDataRest && getDataRest.status == 200 && Array.isArray(getDataRest.data.data)) {
-                console.log('getDataRest.data.data:', getDataRest.data.data)
-                // Render data ke elemen HTML di sini
+            if (getDataRest && getDataRest.status == 200) {
+                let data = getDataRest.data.data;
+                await setData(data);
             } else {
                 let errorMessage = getDataRest?.data?.message || 'Data gagal dimuat';
                 console.error(errorMessage);
             }
+        }
+
+        async function setData(data) {
+            $('#nama_toko').val(data.nama_toko || '');
+            $('#id_transaksi').val(data.id_transaksi || '');
+            $('#tipe_transaksi').val(data.tipe_transaksi || '');
+            $('#nama_member').val(data.nama_member || 'Guest');
+            $('#harga_jual').val(data.harga_jual || 0);
+            $('#nama_barang').val(data.nama_barang || '');
+            $('#qty').val(data.qty_beli || 0);
         }
 
         async function searchData() {
