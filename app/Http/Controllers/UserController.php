c08cc36d2ea430dc067e9b26171135ee9fb0f491
Farditbee <farhandito15@gmail.com>
2024-12-31 13:20:30 +0700

    konz

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
         $this->title = [
             'Data User',
             'Tambah Data',
-            'Edit Data'
+            'Edit Datas'
         ];
     }
 
