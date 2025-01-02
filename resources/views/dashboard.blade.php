785ed44dc70b34f31b72ded6a5c06e5046a79141
Eko748 <permanaeko74@gmail.com>
2025-01-02 12:43:53 +0700

    fix()

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -263,6 +263,11 @@
 
                 total.textContent = formatRupiah(apiResponse.totals || 0);
 
+                if (apiResponse.totals === 0) {
+                    chartContainer.innerHTML = '<p><b class="text-center text-danger">Data tidak ditemukan, silahkan pilih opsi filter lain.</b></p>';
+                    return;
+                }
+
                 const categories = {
                     daily: Array.from({
                         length: penjualan.length
@@ -318,6 +323,7 @@
                 const chart = new ApexCharts(chartContainer, chartOptions);
                 chart.render();
             };
+
 
             setDefaultMonth();
             populateYearOptions();
@@ -375,7 +381,6 @@
                 }
             }
         }
-
 
         function populateYearOptions() {
             const filterYear = document.getElementById('filter-year');
