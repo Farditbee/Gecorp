06a5cd2c5bd8096bb9dec8671f4ce9d62ecd53e2
Bintang0501 <gilangsuryawicaksono@gmail.com>
2025-02-04 09:31:12 +0700

    feat: add endpoint to retrieve barang for pengiriman and update harga barang logic

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Barang;
 use App\Models\DataReture;
+use App\Models\DetailToko;
 use App\Models\Member;
+use App\Models\StockBarang;
 use App\Models\Supplier;
 use App\Models\Toko;
 use Illuminate\Http\Request;
@@ -254,6 +256,68 @@ class MasterController extends Controller
             ];
         }, $data['data']);
 
+        return response()->json([
+            'data' => $mappedData,
+            'status_code' => 200,
+            'errors' => false,
+            'message' => 'Berhasil',
+            'pagination' => $data['meta']
+        ], 200);
+    }
+
+    public function getBarangPengiriman(Request $request)
+    {
+        $meta['orderBy'] = $request->ascending ? 'asc' : 'desc';
+        $meta['limit'] = $request->has('limit') && $request->limit <= 30 ? $request->limit : 30;
+    
+        $id_toko = $request->id_toko;
+    
+        if ($id_toko == 1) {
+            $query = StockBarang::join('barang', 'stock_barang.id_barang', '=', 'barang.id')
+                ->select('stock_barang.id_barang', 'barang.nama_barang', 'stock_barang.stock as qty');
+        } else {
+            $query = DetailToko::join('barang', 'detail_toko.id_barang', '=', 'barang.id')
+                ->where('detail_toko.id_toko', $id_toko)
+                ->select('detail_toko.id_barang', 'barang.nama_barang', 'detail_toko.qty');
+        }
+    
+        if (!empty($request['search'])) {
+            $searchTerm = trim(strtolower($request['search']));
+    
+            $query->where(function ($query) use ($searchTerm) {
+                $query->orWhereRaw("LOWER(barang.nama_barang) LIKE ?", ["%$searchTerm%"]);
+            });
+        }
+    
+        $data = $query->paginate($meta['limit']);
+    
+        $paginationMeta = [
+            'total'        => $data->total(),
+            'per_page'     => $data->perPage(),
+            'current_page' => $data->currentPage(),
+            'total_pages'  => $data->lastPage()
+        ];
+    
+        $data = [
+            'data' => $data->items(),
+            'meta' => $paginationMeta
+        ];
+    
+        if (empty($data['data'])) {
+            return response()->json([
+                'status_code' => 400,
+                'errors' => true,
+                'message' => 'Tidak ada data'
+            ], 400);
+        }
+    
+        $mappedData = array_map(function ($item) {
+            return [
+                'id' => $item['id_barang'],
+                'text' => $item['nama_barang'] . '/' . $item['qty'],
+            ];
+        }, $data['data']);
+    
         return response()->json([
             'data' => $mappedData,
             'status_code' => 200,
