b49db169907c66d44c283792a7ac4b16a1c680f9
Eko748 <permanaeko74@gmail.com>
2025-02-17 09:29:14 +0700

    fix

--- resources/views/reture/index.blade.php
+++ resources/views/reture/index.blade.php
@@ -748,6 +748,7 @@
                 <td class="text-wrap align-top">
                     <span>${data.nama_barang || ''}</span>
                     <input type="hidden" name="id_barang[]" value="${data.id_barang || ''}" class="form-control" readonly required>
+                    <input type="hidden" name="qrcode[]" value="${data.qrcode || ''}" class="form-control" readonly required>
                 </td>
             `;
 
@@ -804,7 +805,6 @@
                 });
             } else if (selectedValue === 'Cash') {}
         }
-
 
         async function getDataQrCode(customFilter3 = {}) {
             let filterParams = {};
@@ -975,7 +975,7 @@
                     const existingPairs = getExistingTransactionItemPairs();
 
                     const isDuplicate = existingPairs.some(pair =>
-                        pair.idBarang == data.id_barang && pair.idTransaksi == data.id_transaksi
+                        pair.idBarang == data.id_barang && pair.idTransaksi == data.id_transaksi && pair.qrCode == data.qrcode
                     );
 
                     if (isDuplicate) {
@@ -985,7 +985,7 @@
                             tbody.removeChild(loadingRow);
                         }
                         notificationAlert('info', 'Pemberitahuan',
-                            'Data dengan Nama Barang dan ID Transaksi ini sudah ada.');
+                            'Data dari QRCode ini sudah ada.');
                         return;
                     } else {
                         await addRowToTable(data);
