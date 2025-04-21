02d2c3ffc9d7d23b791f828cff07543b0319b97b
Bintang0501 <gilangsuryawicaksono@gmail.com>
2025-04-21 09:44:28 +0700

    fix: update UpdatePromoStatus schedule to run every ten minutes

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(UpdatePromoStatus::class)->hourly();
+Schedule::command(UpdatePromoStatus::class)->everyTenMinutes();
 Schedule::command(UpdatePromoStatus::class)->dailyAt('00:00');
