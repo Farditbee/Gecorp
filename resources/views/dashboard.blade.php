5f0633704f48b68ef8328d2068b0f26f0c1adca5
Eko748 <permanaeko74@gmail.com>
2025-05-06 08:54:33 +0700

    fix(dashboard)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -147,8 +147,8 @@
                                                     </form>
                                                 </div>
                                             </div>
-                                            <hr>
                                             @if (Auth::user()->id_level == 1 || Auth::user()->id_level == 5)
+                                            <hr>
                                                 <div class="row">
                                                     <div class="col-md-6 px-3 pb-2">
                                                         <div class="glass flex-fill text-dark">
