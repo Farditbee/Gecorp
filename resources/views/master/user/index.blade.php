3d160c7b795ec4b934d467d779d49f10aa2105d1
Eko748 <permanaeko74@gmail.com>
2025-05-09 10:16:28 +0700

    fix

--- resources/views/master/user/index.blade.php
+++ resources/views/master/user/index.blade.php
@@ -8,6 +8,113 @@
     <link rel="stylesheet" href="{{ asset('css/button-action.css') }}">
     <link rel="stylesheet" href="{{ asset('css/table.css') }}">
     <link rel="stylesheet" href="{{ asset('css/sweetalert2.css') }}">
+    <style>
+        .custom-header {
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-start;
+            flex-wrap: wrap;
+            gap: 10px;
+        }
+
+        .custom-left {
+            display: flex;
+            align-items: center;
+            flex-wrap: wrap;
+            gap: 10px;
+        }
+
+        .custom-btn-tambah-wrap {
+            flex: 1 1 auto;
+        }
+
+        .custom-form-import {
+            display: flex;
+            align-items: center;
+            flex-wrap: wrap;
+            gap: 10px;
+        }
+
+        .custom-input-file {
+            padding: 8px;
+            border: 1px solid #ccc;
+            background-color: #fff;
+            border-radius: 4px;
+            flex: 1 1 auto;
+        }
+
+        .custom-btn-import {
+            flex: 0 0 auto;
+            white-space: nowrap;
+        }
+
+        .custom-right {
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            flex-wrap: wrap;
+            gap: 10px;
+            flex: 0 0 auto;
+        }
+
+        .custom-limit-page {
+            flex: 0 0 auto;
+        }
+
+        .custom-search {
+            flex: 0 0 auto;
+            width: 200px;
+        }
+
+        @media (max-width: 767.98px) {
+            .custom-header {
+                flex-direction: column;
+                align-items: stretch;
+            }
+
+            .custom-left {
+                flex-direction: column;
+                align-items: stretch;
+            }
+
+            .custom-btn-tambah-wrap {
+                width: 100%;
+            }
+
+            .custom-form-import {
+                flex-direction: row;
+                justify-content: space-between;
+                width: 100%;
+            }
+
+            .custom-input-file {
+                flex: 1 1 65%;
+            }
+
+            .custom-btn-import {
+                flex: 1 1 30%;
+            }
+
+            .custom-right {
+                flex-direction: row;
+                justify-content: space-between;
+                width: 100%;
+                margin-top: 10px;
+            }
+
+            .custom-limit-page {
+                flex: 1 1 25%;
+            }
+
+            .custom-search {
+                flex: 1 1 70%;
+            }
+
+            .custom-btn-tambah {
+                width: 100%;
+            }
+        }
+    </style>
 @endsection
 
 @section('content')
@@ -26,6 +133,28 @@
                                         <i class="fa fa-circle-plus"></i> Tambah
                                     </a>
                                 @endif
+                            </div>
+                            <div class="d-flex flex-column flex-lg-row align-items-lg-center justify-content-between mb-2">
+                                <div class="custom-left">
+                                    <div class="custom-btn-tambah-wrap">
+                                        @if (Auth::user()->id_level == 1 || Auth::user()->id_level == 2 || Auth::user()->id_level == 3)
+                                            <a href="{{ route('master.user.create') }}" class="mr-2 btn btn-primary custom-btn-tambah text-white"
+                                                data-container="body" data-toggle="tooltip" data-placement="top"
+                                                title="Tambah Data User">
+                                                <i class="fa fa-circle-plus"></i> Tambah
+                                            </a>
+                                        @endif
+                                    </div>
+                                    <form action="{{ route('master.user.import') }}" method="POST"
+                                        enctype="multipart/form-data" class="custom-form-import">
+                                        @csrf
+                                        <input type="file" name="file" class="custom-input-file" accept=".xlsx"
+                                            required>
+                                        <button type="submit" class="btn btn-success custom-btn-import">
+                                            <i class="fa fa-file-import"></i> Import
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
 
                             <div class="d-flex justify-content-between align-items-center flex-wrap">
